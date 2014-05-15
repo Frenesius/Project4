e0ed181a372a47b170fa3d0e0f08a6b2a6048641
Jiar-PC <0878685@hr.nl>
2014-05-15 23:15:19 +0200

    v0.0.10
    added InputActivity.class

--- Todolist/src/hr/frenesius/todolist/ListActivity.java
+++ Todolist/src/hr/frenesius/todolist/ListActivity.java
@@ -3,16 +3,33 @@ package hr.frenesius.todolist;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
 public class ListActivity extends ActionBarActivity {
+	
+	Button button1; // Button
 
+
+	private int dateday; 
+	private int datemonth;
+	private int dateyear; 
+	private String datestring;
+	private Editable text;
+	private String parsedtext;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,12 +39,87 @@ public class ListActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-	
+		// Buttons voor on click
+				button1 = (Button) findViewById(R.id.inputbutton1);
+				button1.setOnClickListener(button1listener);
+				
+				
 	
 	
 	
 	
 	}
+	// Onclick listener
+		View.OnClickListener button1listener = new View.OnClickListener() {
+			public void onClick(View v) {
+				//Pakt text van een input en viewt het
+				getTexteditText();
+				//Pakt datum van input en viewt het in een textview
+				setDateDatepicker(); 
+			}
+		};
+		
+		
+
+		
+		public void setDateDatepicker() { // On click add calendar view
+			//Pakt datum
+			getDateDatepicker();
+			
+			//Parse date
+			parseDate();
+			
+			//Zet text in textview
+			setDateTextview();
+			
+			//Geeft een Toast terug
+			Toast.makeText(getApplicationContext(), "testDate() triggered",
+					Toast.LENGTH_LONG).show(); // Debug Datum
+		}
+		
+		private void getDateDatepicker(){
+			DatePicker a2 = (DatePicker) findViewById(R.id.inputdatePicker1); // Maakt Datepicker var aan
+			//Get date														
+			dateday = a2.getDayOfMonth(); // Pakt datepicker dag
+			datemonth = a2.getMonth(); // Pakt datepicker maand
+			dateyear = a2.getYear(); // Pakt datepicker jaar	
+		}
+		
+		private void parseDate(){
+			String day = String.valueOf(dateday); // Parsed dag in string
+			String month = String.valueOf(datemonth); // Parsed maand in string
+			String year = String.valueOf(dateyear); // Parsed jaar in string
+			
+			datestring = day + "-" + month + "-" + year; // Maakt er een DD-MM-YYYY van
+
+		}
+		
+		private void setDateTextview(){
+			TextView tv = (TextView) findViewById(R.id.DatetextView); // Pakt textview in een variabele
+			tv.setText(datestring); // Zet de datum in textview
+		}
+		
+		// Pakt text van edittext en zet in view
+		public void getTexteditText() {
+			//Pakt de text uit input
+			getTextText1();
+			//Parsed Editable text in String
+			parseTextText1();
+			//Zet Parsed text in Textview
+			setTextText1();
+		}
+		private void getTextText1(){
+			EditText i = (EditText) findViewById(R.id.inputeditText);
+			text = i.getText();
+		}
+		private void parseTextText1(){
+			parsedtext = text.toString();
+		}
+		private void setTextText1(){
+			TextView a = (TextView) findViewById(R.id.inputtextView);
+			a.setText(parsedtext);
+		}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
