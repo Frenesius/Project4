d0164c96ec58d8e185dfd78ca5357dd44ab35414
J - Arch Linux <0878685@hr.nl>
2014-06-17 01:34:37 +0000

    v0.1.21 -2 \n bugfixes

--- Todolist/gen/hr/frenesius/todolist/R.java
+++ Todolist/gen/hr/frenesius/todolist/R.java
@@ -1101,16 +1101,16 @@ containing a value of this type.
     }
     public static final class id {
         public static final int BadhabitsMain=0x7f050051;
-        public static final int DashboardLinearLayout=0x7f050048;
-        public static final int DashboardMain=0x7f050049;
+        public static final int DashboardLinearLayout=0x7f050046;
+        public static final int DashboardMain=0x7f050047;
         public static final int GoodHabitAction=0x7f050062;
-        public static final int GoodHabitsMain=0x7f05004e;
+        public static final int GoodHabitsMain=0x7f05004d;
         public static final int ListTextview14=0x7f050057;
         public static final int ResetAction=0x7f050065;
         public static final int SAbutton1=0x7f05005f;
         public static final int SAeditText1=0x7f05005e;
-        public static final int YourName=0x7f05004b;
-        public static final int YourScore=0x7f05004d;
+        public static final int YourName=0x7f05004a;
+        public static final int YourScore=0x7f05004c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1134,7 +1134,7 @@ containing a value of this type.
         public static final int container=0x7f05003c;
         public static final int create=0x7f050061;
         public static final int default_activity_button=0x7f050028;
-        public static final int descriptionText1=0x7f050040;
+        public static final int descriptionText1=0x7f050042;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1147,8 +1147,8 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int inputbutton1=0x7f050046;
-        public static final int inputdatePicker1=0x7f050042;
+        public static final int imageView1=0x7f05003d;
+        public static final int inputbutton1=0x7f050044;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1162,8 +1162,8 @@ containing a value of this type.
         public static final int rewardPriceEuro=0x7f05005d;
         public static final int rewardTitle=0x7f050059;
         public static final int reward_activity=0x7f050064;
-        public static final int scrollView1=0x7f050047;
-        public static final int scrollView2=0x7f05003d;
+        public static final int scrollView1=0x7f050045;
+        public static final int scrollView2=0x7f05003e;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1183,21 +1183,21 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int tableReward=0x7f050058;
-        public static final int tableRow1=0x7f05003e;
+        public static final int tableRow1=0x7f050040;
         public static final int tableRow10=0x7f050052;
         public static final int tableRow11=0x7f050054;
         public static final int tableRow12=0x7f050055;
         public static final int tableRow13=0x7f050056;
-        public static final int tableRow2=0x7f050041;
-        public static final int tableRow3=0x7f050045;
-        public static final int tableRow4=0x7f050043;
-        public static final int tableRow5=0x7f05004a;
-        public static final int tableRow6=0x7f05004c;
+        public static final int tableRow2=0x7f05004e;
+        public static final int tableRow3=0x7f050043;
+        public static final int tableRow4=0x7f050049;
+        public static final int tableRow5=0x7f050048;
+        public static final int tableRow6=0x7f05004b;
         public static final int tableRow75=0x7f05004f;
+        public static final int textList=0x7f05003f;
         public static final int textView75=0x7f050050;
-        public static final int timePicker1=0x7f050044;
         public static final int title=0x7f05002b;
-        public static final int titleText1=0x7f05003f;
+        public static final int titleText1=0x7f050041;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
@@ -1311,11 +1311,13 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int create_input=0x7f0a0012;
+        public static final int enter_list=0x7f0a0016;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_input_bad_habit=0x7f0a0013;
         public static final int title_activity_list=0x7f0a0010;
         public static final int title_activity_reward=0x7f0a0014;
         public static final int title_activity_startup=0x7f0a0011;
+        public static final int welkom_start=0x7f0a0015;
     }
     public static final class style {
         /** 
